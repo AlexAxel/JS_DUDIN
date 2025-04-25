ef4ff076e1597fffff05d9bc089a8392d462e3d9
ADudin <alexaxel1991@gmail.com>
2025-04-25 20:11:49 +0300

    test

---
+++ docs/worker/worker-ui.js
@@ -0,0 +1,35 @@
+let worker = null; // воркер (жив/мёртв)
+let activeTasks = 0; // количество активных задач сейчас выполняется
+
+const btn = document.getElementById('start');
+const spinner = document.getElementById('spinner');
+const resultEl = document.getElementById('result');
+
+btn.addEventListener('click', () => {
+    if (!window.Worker) { alert('Web Workers не поддерживаются'); return; }
+
+    if (!worker) { // оздаём воркер только при первом запуске
+        // worker = new Worker('./worker.js', { type: 'module' });
+        const worker = new Worker(new URL('./worker.js', import.meta.url), { type: 'module' });
+
+        worker.onmessage = (e) => {
+            // activeTasks--;
+            spinner.hidden = true;
+            const { iterations, sum } = e.data;
+            resultEl.textContent = `✅ iterations: ${iterations} | sum: ${sum}`;
+
+            if (--activeTasks === 0) { // когда задач не осталось - убиваем воркер
+                worker.terminate();
+                worker = null; // позволит создать нового на след. клике
+            }
+        };
+
+        worker.onerror = (err) => console.error('worker error', err);
+    }
+
+    // отправляем новую задачу текущему (или новому) воркеру
+    activeTasks++;
+    spinner.hidden = false;
+    resultEl.textContent = '';
+    worker.postMessage({ timeout: 3000 });
+});
---
+++ docs/worker/worker.js
@@ -0,0 +1,15 @@
+// получает timeout, выполняет медленную функцию и шлёт результат
+const slowFunction = (timeout = 3000) => {
+    const start = performance.now();
+    let x = 0, i = 0;
+
+    do { i += 1; x += (Math.random() - .5) * i; }
+    while (performance.now() - start < timeout);
+
+    return { iterations: i, sum: x.toFixed(2) };
+};
+
+self.onmessage = (e) => {
+    const ms = e.data?.timeout ?? 3000;
+    self.postMessage(slowFunction(ms));
+};
